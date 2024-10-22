74a60ad75a1b458bffdda55e07d6ea437809c4fe
Praveen0117 <polakipraveenkumar9@gmail.com>
2024-10-22 09:54:25 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="font-serif font-bold text-2xl text-blue-700">Praveen kumar Polaki</h1>
+    </div>
+  );
+}
+
+export default App;
